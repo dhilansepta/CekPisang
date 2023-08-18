1c9ae9e9e3b0beac9236b9156e4bbf8fa7931156
hfdzz <yhafidz@gmail.com>
2023-08-18 14:11:50 +0700

    add - HomeUnit

--- App.js
+++ App.js
@@ -4,9 +4,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import * as SplashScreen from 'expo-splash-screen';
 import Welcome from "./components/screens/Login";
+import Unit from "./components/screens/HomeUnit";
 
-SplashScreen.preventAutoHideAsync();
-setTimeout(SplashScreen.hideAsync, 1000);
+// SplashScreen.preventAutoHideAsync();
+// setTimeout(SplashScreen.hideAsync, 1000);
 
 const App = () => {
   const Stack = createNativeStackNavigator();
@@ -19,6 +20,7 @@ const App = () => {
         }}
       >
         <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="HomeUnit" component={Unit} />
       </Stack.Navigator>
     </NavigationContainer>
   );
