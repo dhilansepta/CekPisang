e6a6095b1344c8e85d5a2809879c0464b89c6fb1
hfdzz <yhafidz@gmail.com>
2023-08-23 14:28:48 +0700

    add - unit & ops sample data

---
+++ assets/sample_data/unit_sample_data.js
@@ -0,0 +1,36 @@
+const unit_sample_data = 
+{
+    QRIMAGE: require('../../components/storage/images/qrcode_example.png'),
+    id: '1306231c07230069tuA6',
+    timestamp: 'Wed Jul 27 2023 09:50:05 GMT+0700 (Western Indonesia Time)',
+    desc: 
+        {
+        Berat: 2534,
+        Warna: 'Hijau',
+        Varietas: 'Pisang Ambon',
+        },
+    dataLainnya: '',
+    dataProduksi: 
+    {
+        TanggalPanen: '26 Juli 2023',
+        TanggalKadaluarsa: '26 Agustus 2023',
+        WaktuPengiriman: 'Kamis, 27 Juli 2023'
+    },
+    dataTransformasi: 
+    {
+        LokasiAwal:'Kebun Pisang Nusa Indah, Kebun 2, Kec. Tanah TInggi',
+        LokasiAkhir:'Gudang Pisang PT Pisang Enak, Jl. Perwira, Kec. Tanah Rendah',
+    },
+    idUnitInput: [
+        {
+        id: '1306231c07230069tuA5',
+        weight: 1900
+        },
+        {
+        id: '1306231c07230069tuA4',
+        weight: 2421
+        }
+    ]
+}
+export {unit_sample_data};
+
